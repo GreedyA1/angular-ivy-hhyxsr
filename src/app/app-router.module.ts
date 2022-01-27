4222ffbcbf664bc5f84ba97e9b10b87e4bc03aec
GreedyAi <m.vato122@gmail.com>
2022-01-27 19:24:24 +0400

    news

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './new/news/news.component';
+
+const routes: Routes = [
+  {
+    path: 'news',
+    component: NewsComponent,
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRouterModule { }
+export class AppRoutingModule { }
